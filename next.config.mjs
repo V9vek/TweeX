a3a18733107b50f7043aa00a319e05f4dacba473
Vivek Sharma <viveksharma2382000@gmail.com>
2024-08-08 16:40:08 +0530

    feat: installed dep

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+/**
+ * In NextJS 15, Client side router does not cache pages at client side, for like 30 sec
+ * It was not likely, as user will see STALE data for like 30 seconds
+ * But in this project, we are using React Query to refresh the feed, and the other data is not changing
+ * much, so enabling that client side router caching
+ */
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
+};
 
 export default nextConfig;
