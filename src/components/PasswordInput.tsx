88122b5942f6a583078810c7f5ed13a6314f569c
Vivek Sharma <viveksharma2382000@gmail.com>
2024-08-09 16:25:59 +0530

    feat: auth frontend

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Input, InputProps } from "./ui/input";
+import { cn } from "@/lib/utils";
+import { Eye, EyeOff } from "lucide-react";
+
+const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false);
+    
+    return (
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          className={cn(
+            "pe-10 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {props.value && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            title={showPassword ? "Hide password" : "Show password"}
+            className="absolute right-3 top-1/2 -translate-y-1/2 transform text-muted-foreground"
+          >
+            {showPassword ? (
+              <EyeOff className="size-5" />
+            ) : (
+              <Eye className="size-5" />
+            )}
+          </button>
+        )}
+      </div>
+    );
+  }
+);
+PasswordInput.displayName = "PasswordInput";
+
+export { PasswordInput };
