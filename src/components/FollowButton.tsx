9b8be25826a916cbe73dbc288a7056f1b8ba7b9b
Vivek Sharma <viveksharma2382000@gmail.com>
2024-08-15 23:51:53 +0530

    feat: follow feature with optimistic update

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { FollowerInfo } from "@/lib/types";
+import { Button } from "./ui/button";
+import useFollowerInfo from "@/hooks/useFollowerInfo";
+import { QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
+import kyInstance from "@/lib/ky";
+import { useToast } from "./ui/use-toast";
+
+interface FollowButtonProps {
+  userId: string;
+  initialState: FollowerInfo;
+}
+
+export default function FollowButton({
+  userId,
+  initialState,
+}: FollowButtonProps) {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const { data } = useFollowerInfo(userId, initialState);
+  console.log(data);
+
+  const queryKey: QueryKey = ["follower-info", userId];
+
+  const mutation = useMutation({
+    mutationFn: () =>
+      data.isFollowedByUser
+        ? kyInstance.delete(`/api/users/${userId}/followers`)
+        : kyInstance.post(`/api/users/${userId}/followers`),
+    // onMutate() is called before the mutationFn()
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousState = queryClient.getQueryData<FollowerInfo>(queryKey);
+
+      queryClient.setQueryData<FollowerInfo>(queryKey, () => ({
+        followers:
+          (previousState?.followers || 0) +
+          (previousState?.isFollowedByUser ? -1 : 1),
+        isFollowedByUser: !previousState?.isFollowedByUser,
+      }));
+
+      return { previousState };
+    },
+    onError(error, variables, context) {
+      // if incase of error, rollback to previous state
+      queryClient.setQueryData(queryKey, context?.previousState);
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    },
+  });
+
+  return (
+    <Button
+      variant={data.isFollowedByUser ? "secondary" : "default"}
+      onClick={() => mutation.mutate()}
+    >
+      {data.isFollowedByUser ? "Unfollow" : "Follow"}
+    </Button>
+  );
+}
--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -1,6 +1,5 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDataSelect } from "@/lib/types";
 import Link from "next/link";
 import UserAvatar from "./UserAvatar";
 import { Button } from "./ui/button";
@@ -8,6 +7,8 @@ import { Suspense } from "react";
 import { Loader2 } from "lucide-react";
 import { unstable_cache } from "next/cache";
 import { formatNumber } from "@/lib/utils";
+import FollowButton from "./FollowButton";
+import { getUserDataSelect } from "@/lib/types";
 
 export default function TrendsSidebar() {
   return (
@@ -24,42 +25,55 @@ export default function TrendsSidebar() {
 async function WhoToFollow() {
   const { user } = await validateRequest();
 
-//   await new Promise((r) => setTimeout(r, 10000));
+  //   await new Promise((r) => setTimeout(r, 10000));
 
   if (!user) return null;
 
   const usersToFollow = await prisma.user.findMany({
     where: {
       NOT: { id: user.id },
+      // do not show those people to follow, whom I have already followed
+      followers: {
+        none: {
+          followerId: user.id,
+        },
+      },
     },
-    select: userDataSelect,
+    select: getUserDataSelect(user.id),
     take: 5,
   });
 
   return (
     <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="text-lg font-bold">Who to follow</div>
-      {usersToFollow.map((user) => {
+      {usersToFollow.map((u) => {
+        // console.log(u);
         return (
-          <div
-            key={user.id}
-            className="flex items-center justify-between gap-3"
-          >
+          <div key={u.id} className="flex items-center justify-between gap-3">
             <Link
-              href={`/users/${user.username}`}
+              href={`/users/${u.username}`}
               className="flex items-center gap-3"
             >
-              <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+              <UserAvatar avatarUrl={u.avatarUrl} className="flex-none" />
               <div>
                 <p className="line-clamp-1 break-all font-semibold hover:underline">
-                  {user.displayName}
+                  {u.displayName}
                 </p>
                 <p className="line-clamp-1 break-all text-muted-foreground">
-                  @{user.username}
+                  @{u.username}
                 </p>
               </div>
             </Link>
-            <Button>Follow</Button>
+            <FollowButton
+              key={u.id}
+              userId={u.id}
+              initialState={{
+                followers: u._count.followers,
+                isFollowedByUser: u.followers.some(
+                  ({ followerId }) => followerId === user.id
+                ),
+              }}
+            />
           </div>
         );
       })}
