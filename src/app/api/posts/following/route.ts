5e713a528ce12fceec3fb077b4d9aadb0de04cdd
Vivek Sharma <viveksharma2382000@gmail.com>
2024-08-16 11:18:37 +0530

    feat: following feed

---
+++ src/app/api/posts/following/route.ts
@@ -0,0 +1,47 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude, PostsPage } from "@/lib/types";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    // cursor here is the next page, from where we want to start fetching "pageSize" posts
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const pageSize = 5;
+
+    const { user } = await validateRequest();
+
+    if (!user) {
+      return Response.json({ error: "Unauthorized user" }, { status: 401 });
+    }
+
+    const posts = await prisma.post.findMany({
+      where: {
+        user: {
+          followers: {
+            some: {
+              followerId: user.id,
+            },
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
+      include: getPostDataInclude(user.id),
+    });
+
+    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, pageSize),
+      nextCursor: nextCursor,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
